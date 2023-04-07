50af537b428dba0effdf762a51260cf4bf5b6cf6
gizeli-pereira <gizeli.pereira1@gmail.com>
2023-04-07 14:22:04 -0500

    Define basic Schema for posts

---
+++ models/posts.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+    title: {String,
+    required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        // required: true
+    },
+    userName: String,
+    userAvatar: String
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Post', postSchema);
